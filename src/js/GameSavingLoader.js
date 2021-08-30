e6ae09ac53fc610fd88424f0827fbbb6d6fc8c44
kxrxll <kovalyovkirill@gmail.com>
2021-08-30 22:52:31 +0300

    solved

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,7 +2,9 @@ import read from './reader';
 import json from './parser';
 
 export default class GameSavingLoader {
-  static load() {
-    return read().then((result) => json(result).then((response) => response));
+  static async load() {
+    const data = await read();
+    const value = await json(data);
+    return value;
   }
 }
