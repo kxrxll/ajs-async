4c73d9eb4b74f8278cd31151116db104bc494b77
kxrxll <kovalyovkirill@gmail.com>
2021-09-08 10:00:21 +0300

    once again

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import GameSavingLoader from './GameSavingLoader';
 
-export default async () => {
+(async function gameSavingLoader() {
   try {
     const data = await GameSavingLoader.load();
     return data;
   } catch (err) {
     throw new Error(err);
   }
-};
+}());
